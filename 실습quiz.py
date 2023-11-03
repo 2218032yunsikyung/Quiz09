b10a649035b646d90069218db29a2ffc699a3247
2218032yunsikyung <144421368+2218032yunsikyung@users.noreply.github.com>
2023-11-03 16:10:43 +0900

    Update 실습quiz.py

--- 실습quiz.py
+++ 실습quiz.py
@@ -1,18 +1,16 @@
-tree_height = int(input("크리스마스 트리의 높이를 설정하세요: "))
+트리_높이 = int(input("크리스마스 트리의 높이를 입력하세요: "))
 
-for i in range(tree_height):
-    spaces = tree_height - i - 1
-    stars = 2 * i + 1
+for i in range(트리_높이):
+    공백 = 트리_높이 - i - 1
+    별표 = 2 * i + 1
 
-
-    for j in range(spaces):
+    for j in range(공백):
         print(' ', end='')
 
-    for j in range(stars):
+    for j in range(별표):
         print('*', end='')
 
     print()
-
 
 
 
